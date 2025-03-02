e2481e4b58528a67983fb089d1850a3d774cd6fd
Butorin Vasiliy <vasilyman@mail.ru>
2025-03-02 21:15:44 +0500

    wip: add login form

---
+++ src/features/signIn/index.tsx
@@ -0,0 +1,37 @@
+import type { FC } from 'react';
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import { LoginForm } from '../forms/loginForm';
+import { Button } from '../../shared/button';
+import $style from './style.module.scss';
+import type { LoginFormData } from '../forms/loginForm/type';
+
+export const SignIn: FC = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<LoginFormData>({
+    defaultValues: {
+      email: 'initial@value',
+      password: 'initial value',
+    },
+    mode: 'all',
+  });
+
+  const onSubmit = async (data: LoginFormData) => {
+    const res = await new Promise((res) => {
+      setTimeout(() => {
+        res(data);
+      }, 1000);
+    });
+    console.log(res);
+  };
+
+  return (
+    <div className={$style['sign-in']}>
+      <LoginForm control={control} />
+      <Button label="Войти" block disabled={isSubmitting} onClick={handleSubmit(onSubmit)} />
+    </div>
+  );
+};
