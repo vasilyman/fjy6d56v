83c941aabf7e7bdbe13fc27485799f0ce2bc3105
Butorin Vasiliy <vasilyman@mail.ru>
2025-03-18 22:36:46 +0500

    fix: router basename

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -2,29 +2,32 @@ import { createBrowserRouter } from 'react-router';
 import { IndexPage } from 'src/pages/indexPage';
 import { Layout } from '../layout';
 
-export const router = createBrowserRouter([
-  {
-    path: '/',
-    Component: Layout,
-    children: [
-      {
-        path: '/',
-        Component: IndexPage,
-      },
-      {
-        path: 'cart',
-        lazy: async () => {
-          const { CartPage: Component } = await import('src/pages/cartPage');
-          return { Component };
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      Component: Layout,
+      children: [
+        {
+          path: '/',
+          Component: IndexPage,
         },
-      },
-      {
-        path: 'me',
-        lazy: async () => {
-          const { ProfilePage: Component } = await import('src/pages/profilePage');
-          return { Component };
+        {
+          path: 'cart',
+          lazy: async () => {
+            const { CartPage: Component } = await import('src/pages/cartPage');
+            return { Component };
+          },
         },
-      },
-    ],
-  },
-]);
+        {
+          path: 'me',
+          lazy: async () => {
+            const { ProfilePage: Component } = await import('src/pages/profilePage');
+            return { Component };
+          },
+        },
+      ],
+    },
+  ],
+  { basename: '/fjy6d56v' }
+);
